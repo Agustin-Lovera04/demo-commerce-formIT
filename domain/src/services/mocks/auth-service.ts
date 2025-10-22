a8b3056d4a9f378a3ba02dfdd591e0a41dfb003e
Agustin Lovera <agusslovera04@gmail.com>
2025-10-22 00:53:08 -0300

    work in auth service mock

---
+++ domain/src/services/mocks/auth-service.ts
@@ -0,0 +1,43 @@
+import { IUser } from "../../entities";
+import { authenticationService } from "../authentication/auth-service";
+import { Response } from "../../utils/index";
+import jwt from 'jsonwebtoken';
+import { config } from "../../config/config";
+import { BaseServiceMock } from "./base-service-mock";
+
+export class AuthenticationServiceMock extends BaseServiceMock<IUser> implements authenticationService {
+    constructor(initialUsers: IUser[] = []) {
+        super(initialUsers)
+    }
+    async findUserByEmail(email: string): Promise<Response<IUser>> {
+        const user = this.items.find(u => u.email === email);
+        if (!user) {
+            return { success: false, error: 'User not found' };
+        }
+        return { success: true, data: user };
+    }
+
+    async validEmail(email: string): Promise<Response<boolean>> {
+        let exReg = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+        let valid = exReg.test(email);
+        if (!valid) return { success: false, error: 'Invalid email' };
+        return { success: true, data: true };
+    }
+
+
+    async validPassword(password: string, existUserInDB: IUser): Promise<Response<IUser>> {
+        if (password !== existUserInDB.password) {
+            return { success: false, error: "Invalid password" };
+        }
+        return { success: true, data: existUserInDB };
+    }
+
+    async generateTokenUser(dataUser: Omit<IUser, "password">): Promise<Response<string>> {
+        const token = jwt.sign(
+            dataUser,
+            config.SECRET_KEY_JWT,
+            { expiresIn: '1h' }
+        );
+        return { success: true, data: token };
+    }
+}
