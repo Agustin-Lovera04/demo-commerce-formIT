5bb968e6b91eb158ab115a621e9370f02be148f9
Agustin Lovera <agusslovera04@gmail.com>
2025-10-22 11:27:06 -0300

    barrels files in products uses cases

---
+++ domain/src/uses-cases/products/index.ts
@@ -0,0 +1,6 @@
+export * from './create-product.js'
+export * from './delete-products.js'
+export * from './edit-products.js'
+export * from './get-all-products.js'
+export * from './get-product-by-id.js'
+
