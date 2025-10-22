570e7d18a7af13a9b73bdeb88d39d90dfedac103
Agustin Lovera <agusslovera04@gmail.com>
2025-10-22 09:40:15 -0300

    resolve register module TDD

--- domain/src/uses-cases/authentication/register.test.ts
+++ domain/src/uses-cases/authentication/register.test.ts
@@ -35,67 +35,73 @@ describe('Register', () => {
         }
     })
 
-   test('Receives user data with email already existing in the database and should return an error', async () => {
-         const result = await registerUser({
-             dependencies: { authenticationService },
-             payload: {
-                 id: 'idUnique',
-                 email: 'test@gmail.com',
-                 password: 'test',
-                 name: 'Agustin',
-                 role: UserRole.CLIENT
-             }
-         })
- 
+    test('Receives user data with email already existing in the database and should return an error', async () => {
+        const result = await registerUser({
+            dependencies: { authenticationService },
+            payload: {
+                id: 'idUnique',
+                email: 'test@gmail.com',
+                password: 'test',
+                name: 'Agustin',
+                role: UserRole.CLIENT
+            }
+        })
+
         expect(result.success).toBe(false)
         if (!result.success) {
             expect(result.error).toStrictEqual('User already exists')
         }
-     })
- 
-        test('Receives user data with email already existing in DB and should return an error', async () => {
-         const result = await registerUser({
-             dependencies: { authenticationService },
-             payload: {
-                 id: 'idUnique',
-                 email: 'test2@gmail.com',
-                 password: '',
-                 name: 'Agustin',
-                 role: UserRole.CLIENT
-             }
-         })
- 
-         expect(result).toStrictEqual('Invalid password')
-     })
- 
- /*    
-     test('Receives user data with invalid email and should return an error', async () => {
-         const result = await registerUser({
-             dependencies: { authenticationService },
-             payload: {
-                 id: 'idUnique',
-                 email: 'test',
-                 password: 'test',
-                 name: 'Agustin',
-                 role: UserRole.CLIENT
-             }
-         })
- 
-         expect(result).toStrictEqual('Invalid email')
-     })
-         
-     test('Receives user data with role = ADMIN and should create and return the same', async () => {
-         const result = await registerUser({
-             dependencies: { authenticationService },
-             payload: {
-                 id: 'idADMIN',
-                 email: 'ADMIN@gmail.com',
-                 password: 'ADMIN',
-                 name: 'ADMIN',
-                 role: UserRole.ADMIN
-             }
-         })
- 
-         expect(result).toHaveProperty('role', 'ADMIN');
-     }) */
+    })
+
+    test('Receives user data with email already existing in DB and should return an error', async () => {
+        const result = await registerUser({
+            dependencies: { authenticationService },
+            payload: {
+                id: 'idUnique',
+                email: 'test2@gmail.com',
+                password: '',
+                name: 'Agustin',
+                role: UserRole.CLIENT
+            }
+        })
+        expect(result.success).toBe(false)
+        if (!result.success) {
+            expect(result.error).toStrictEqual('Invalid password')
+        }
+    })
+
+
+    test('Receives user data with invalid email and should return an error', async () => {
+        const result = await registerUser({
+            dependencies: { authenticationService },
+            payload: {
+                id: 'idUnique',
+                email: 'test',
+                password: 'test',
+                name: 'Agustin',
+                role: UserRole.CLIENT
+            }
+        })
+        expect(result.success).toBe(false)
+        if (!result.success) {
+            expect(result.error).toStrictEqual('Invalid email')
+        }
+    })
+
+    test('Receives user data with role = ADMIN and should create and return the same', async () => {
+        const result = await registerUser({
+            dependencies: { authenticationService },
+            payload: {
+                id: 'idADMIN',
+                email: 'ADMIN@gmail.com',
+                password: 'ADMIN',
+                name: 'ADMIN',
+                role: UserRole.ADMIN
+            }
+        })
+        expect(result.success).toBe(true)
+        if (result.success) {
+            expect(result.data).toHaveProperty('role', 'ADMIN');
+        }
+    })
 })
