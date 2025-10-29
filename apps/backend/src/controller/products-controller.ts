5c180ac2f6e970d14d8d0792fa0efca915c33dfc
Agustin Lovera <agusslovera04@gmail.com>
2025-10-29 08:24:30 -0300

    productController controller/router

---
+++ apps/backend/src/controller/products-controller.ts
@@ -0,0 +1,106 @@
+import { Request, Response } from "express";
+import { ProductsServiceReal } from "../infraestructure/services/products/products-service";
+import { createProduct, deleteProducts, editProducts, getAllProducts, getProductById } from "../../../../domain/dist";
+
+const productsService = new ProductsServiceReal();
+
+export class ProductsController {
+    static async getAllProducts(req: Request, res: Response) {
+        try {
+            const result = await getAllProducts({
+                dependencies: productsService,
+            });
+
+            if (!result.success) {
+                return res.status(400).json({ error: result.error });
+            }
+
+            return res.status(201).json({ products: result.data });
+        } catch (error) {
+            return res.status(500).json({ error: "Internal server error" });
+        }
+    }
+
+    static async getProductById(req: Request, res: Response) {
+        try {
+            const { id } = req.params
+            if (!id) {
+                res.setHeader('Content-Type', 'application/json');
+                return res.status(404).json({ error: 'Missing id' });
+            }
+            const result = await getProductById({
+                dependencies: productsService,
+                payload: { id }
+            });
+
+            if (!result.success) {
+                return res.status(400).json({ error: result.error });
+            }
+
+            return res.status(201).json({ product: result.data });
+        } catch (error) {
+            return res.status(500).json({ error: "Internal server error" });
+        }
+    }
+
+    static async createProduct(req: Request, res: Response) {
+        try {
+            const result = await createProduct({
+                dependencies: productsService,
+                payload: req.body
+            });
+
+            if (!result.success) {
+                return res.status(400).json({ error: result.error });
+            }
+
+            return res.status(201).json({ product: result.data });
+        } catch (error) {
+            return res.status(500).json({ error: "Internal server error" });
+        }
+    }
+
+    static async editProduct(req: Request, res: Response) {
+        try {
+            const { id } = req.params
+            if (!id) {
+                res.setHeader('Content-Type', 'application/json');
+                return res.status(404).json({ error: 'Missing id' });
+            }
+            const result = await editProducts({
+                dependencies: productsService,
+                payload: { id, ...req.body }
+            });
+
+            if (!result.success) {
+                return res.status(400).json({ error: result.error });
+            }
+
+            return res.status(201).json({ product: result.data });
+        } catch (error) {
+            return res.status(500).json({ error: "Internal server error" });
+        }
+    }
+
+    static async deleteProduct(req: Request, res: Response) {
+        try {
+            const { id } = req.params
+            if (!id) {
+                res.setHeader('Content-Type', 'application/json');
+                return res.status(404).json({ error: 'Missing id' });
+            }
+            const result = await deleteProducts({
+                dependencies: productsService,
+                payload: { id }
+            });
+
+            if (!result.success) {
+                return res.status(400).json({ error: result.error });
+            }
+
+            return res.status(201).json({ product: result.data });
+        } catch (error) {
+            return res.status(500).json({ error: "Internal server error" });
+        }
+    }
+}
--- apps/backend/src/routes/products-router.ts
+++ apps/backend/src/routes/products-router.ts
@@ -1,109 +1,18 @@
-import { Router, Request, Response } from "express";
-import { ProductsServiceReal } from "../infraestructure/services/products/products-service";
-import { createProduct, deleteProducts, editProducts, getAllProducts, getProductById } from "../../../../domain/dist";
+import { Router } from 'express'
 import { accessControl } from "../middleware/accessControl";
 import { jwtValidate } from "../middleware/jwtValidate";
+import { ProductsController } from 'src/controller/products-controller';
 
 export const router = Router();
-const productsService = new ProductsServiceReal();
 
-router.get("/getAllProducts", jwtValidate, accessControl(['CLIENT', 'ADMIN']),async (req: Request, res: Response) => {
-  try {
-    const result = await getAllProducts({
-      dependencies: productsService,
-    });
-    
-    if (!result.success) {
-      return res.status(400).json({ error: result.error });
-    }
+router.get("/getAllProducts", jwtValidate, accessControl(['CLIENT', 'ADMIN']), ProductsController.getAllProducts);
 
-    return res.status(201).json({ products: result.data });
-  } catch (error) {
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
+router.get("/getProduct/:id", jwtValidate, accessControl(['CLIENT', 'ADMIN']), ProductsController.getProductById);
 
-router.get("/getProduct/:id", jwtValidate, accessControl(['CLIENT', 'ADMIN']),async (req: Request, res: Response) => {
-  try {
-    const {id} = req.params
-    if(!id){
-      res.setHeader('Content-Type','application/json');
-      return res.status(404).json({error: 'Missing id'});
-    }
-    const result = await getProductById({
-      dependencies: productsService,
-      payload: {id}
-    });
-    
-    if (!result.success) {
-      return res.status(400).json({ error: result.error });
-    }
+router.post("/createProduct", jwtValidate, accessControl(['ADMIN']), ProductsController.createProduct);
 
-    return res.status(201).json({ product: result.data });
-  } catch (error) {
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
+router.put("/editProduct/:id", jwtValidate, accessControl(['ADMIN']), ProductsController.editProduct);
 
-router.post("/createProduct", jwtValidate, accessControl(['ADMIN']),async (req: Request, res: Response) => {
-  try {
-    const result = await createProduct({
-      dependencies: productsService,
-      payload: req.body
-    });
-    
-    if (!result.success) {
-      return res.status(400).json({ error: result.error });
-    }
-
-    return res.status(201).json({ product: result.data });
-  } catch (error) {
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-router.put("/editProduct/:id", jwtValidate, accessControl(['ADMIN']),async (req: Request, res: Response) => {
-  try {
-    const {id} = req.params
-    if(!id){
-      res.setHeader('Content-Type','application/json');
-      return res.status(404).json({error: 'Missing id'});
-    }
-    const result = await editProducts({
-      dependencies: productsService,
-      payload: {id, ...req.body}
-    });
-    
-    if (!result.success) {
-      return res.status(400).json({ error: result.error });
-    }
-
-    return res.status(201).json({ product: result.data });
-  } catch (error) {
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-router.delete("/deleteProduct/:id", jwtValidate, accessControl(['ADMIN']),async (req: Request, res: Response) => {
-  try {
-    const {id} = req.params
-    if(!id){
-      res.setHeader('Content-Type','application/json');
-      return res.status(404).json({error: 'Missing id'});
-    }
-    const result = await deleteProducts({
-      dependencies: productsService,
-      payload: {id}
-    });
-    
-    if (!result.success) {
-      return res.status(400).json({ error: result.error });
-    }
-
-    return res.status(201).json({ product: result.data });
-  } catch (error) {
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
+router.delete("/deleteProduct/:id", jwtValidate, accessControl(['ADMIN']), ProductsController.deleteProduct);
 
 export default router;
